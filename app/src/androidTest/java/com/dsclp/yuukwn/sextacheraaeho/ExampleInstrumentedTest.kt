d4bc2ce66f1195053dcde2ee41f75b8d547662ef
logonrm <logonrm@fiap.com.br>
2018-02-09 10:15:58 -0200

    Initial commit

---
+++ app/src/androidTest/java/com/dsclp/yuukwn/sextacheraaeho/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.dsclp.yuukwn.sextacheraaeho
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.dsclp.yuukwn.sextacheraaeho", appContext.packageName)
+    }
+}
